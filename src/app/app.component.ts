80e3eabb2b40836e191e43df7a7166607e70ee2a
mariemhallabia <mariem@formanet.tn>
2024-06-13 17:28:31 +0200

    language translate et component site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-
+import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');}
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   standalone: true,
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet,TranslateModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
 export class AppComponent {
-  constructor() {}
+  constructor(public translate: TranslateService) {
+    this.translate.setDefaultLang('ar'); 
+
+  }
+  changeLanguage(language: string) {
+    this.translate.use(language);
+}
+
 }
--- src/app/features/generic-site/generic-site.component.ts
+++ src/app/features/generic-site/generic-site.component.ts
@@ -1,14 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { IonButton, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-generic-site',
   templateUrl: './generic-site.component.html',
   styleUrls: ['./generic-site.component.scss'],
+  standalone: true,
+  imports: [IonButton, IonContent, IonicModule, CommonModule,TranslateModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
 export class GenericSiteComponent  implements OnInit {
 
   constructor() { }
 
   ngOnInit() {}
-
+  
 }
