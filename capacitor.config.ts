e5d2313a046415a1b336ad55537cd63b47b4b383
Mahdi Chiha <chihaa.mahdi@gmail.com>
2024-06-20 10:48:24 +0100

    upd

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'Venus',
+  appId: 'cmam.tn',
+  appName: 'VENUS',
   webDir: 'www'
 };
 
